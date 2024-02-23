55b7ee70974dc24be5ddfca4498a212a3a1a5862
Aleksandr Belousov <lksndrbelousov1983@mail.ru>
2024-02-23 10:24:50 +0700

    Adding first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,13 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let disc = b * b - 4 * a * c;
+  if (disc > 0) {
+    arr.push((-b + Math.sqrt(disc)) / 2 / a);
+    arr.push((-b - Math.sqrt(disc)) / 2 / a);
+  } else if (disc == 0) {
+    arr.push(-b / 2 / a);
+  }
   
   return arr;
 }
